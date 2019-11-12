3f57577c4cb0cb798dbf4b56371344b81f1a4933
kobe-bogaerts <kobe.bogaerts@student.ap.be>
2019-11-12 17:03:31 +0100

    data van json word in een array van timetableitem objecten gestoken

--- app/src/main/java/com/kolllor3/lijnhaltecopanian/model/TimeTableItem.java
+++ app/src/main/java/com/kolllor3/lijnhaltecopanian/model/TimeTableItem.java
@@ -1,12 +1,97 @@
 package com.kolllor3.lijnhaltecopanian.model;
 
-import java.time.LocalTime;
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
+import java.time.LocalTime;
+import java.util.Calendar;
+
+@Entity(tableName = "timeTable")
 public class TimeTableItem {
 
+    @PrimaryKey(autoGenerate = true)
+    private int id;
     private int haltenummer;
     private int lijnnummer;
-    private LocalTime time;
+    private int hour;
+    private int minute;
     private int dayOfWeek;
     private String bestemming;
+    private boolean isVacation;
+
+    @Ignore
+    public TimeTableItem(int haltenummer, int lijnnummer, int hour, int minute, int dayOfWeek, String bestemming, boolean isVacation) {
+        this.haltenummer = haltenummer;
+        this.lijnnummer = lijnnummer;
+        this.hour = hour;
+        this.minute = minute;
+        this.dayOfWeek = dayOfWeek;
+        this.bestemming = bestemming;
+        this.isVacation = isVacation;
+    }
+
+    public int getHaltenummer() {
+        return haltenummer;
+    }
+
+    public void setHaltenummer(int haltenummer) {
+        this.haltenummer = haltenummer;
+    }
+
+    public int getLijnnummer() {
+        return lijnnummer;
+    }
+
+    public void setLijnnummer(int lijnnummer) {
+        this.lijnnummer = lijnnummer;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(int dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public String getBestemming() {
+        return bestemming;
+    }
+
+    public void setBestemming(String bestemming) {
+        this.bestemming = bestemming;
+    }
+
+    public boolean isVacation() {
+        return isVacation;
+    }
+
+    public void setVacation(boolean vacation) {
+        isVacation = vacation;
+    }
+
+    @Ignore
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format("%d, lijn: %d, %d%d, %s || %s", haltenummer, lijnnummer, hour, minute, dayOfWeek, bestemming);
+    }
 }
--- app/src/main/java/com/kolllor3/lijnhaltecopanian/providers/LijnApiProider.java
+++ app/src/main/java/com/kolllor3/lijnhaltecopanian/providers/LijnApiProider.java
@@ -8,28 +8,46 @@ import com.kolllor3.lijnhaltecopanian.constants.Constants;
 import com.kolllor3.lijnhaltecopanian.model.TimeTableItem;
 import com.kolllor3.lijnhaltecopanian.util.LijnCustomRequest;
 import com.kolllor3.lijnhaltecopanian.util.LogUtils;
+import com.kolllor3.lijnhaltecopanian.util.Utilities;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class LijnApiProider implements Constants {
 
-    LiveData<List<TimeTableItem>> timeTable = new MutableLiveData<>();
+    MutableLiveData<List<TimeTableItem>> timeTable = new MutableLiveData<>();
 
     public void getDienstRegeling(int halteentiteit, int haltenummer){
         LijnCustomRequest request = new LijnCustomRequest(API_HALE_URL.concat(String.valueOf(halteentiteit)).concat("/").concat(String.valueOf(haltenummer)).concat(DIENSTREGELING_PATH), null, response -> {
             try {
+                List<TimeTableItem> timeTableForHalte = new ArrayList<>();
                 JSONArray doorkomstenArray = response.getJSONArray("doorkomsten");
                 LogUtils.logE("response", doorkomstenArray.toString());
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-ddTkk:mm:ss", Locale.getDefault());
+                Calendar c = Calendar.getInstance();
+
                 //todo: create timetable
                 for (int i = 0; i < doorkomstenArray.length(); i++) {
                     JSONObject object = doorkomstenArray.getJSONObject(i);
-                    
+                    Date date = dateFormat.parse(object.getString("dienstregelingTijdstip"));
+                    if (Utilities.isNotNull(date)) {
+                        c.setTime(date);
+                        timeTableForHalte.add(new TimeTableItem(haltenummer, object.getInt("lijnnummer"), c.get(Calendar.HOUR), c.get(Calendar.MINUTE), c.get(Calendar.DAY_OF_WEEK), object.getString("bestemming"), false));
+                    }
                 }
-            } catch (JSONException e) {
+
+                timeTable.setValue(timeTableForHalte);
+            } catch (JSONException | ParseException e) {
                 e.printStackTrace();
             }
         }, error -> {
